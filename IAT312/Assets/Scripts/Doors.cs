f4a60197d55bee73c63ae66a244261b2e393ee67
jnarlin <jjn9@sfu.ca>
2023-10-23 00:20:32 -0700

    Add empty project

---
+++ IAT312/Assets/Scripts/Doors.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public Animator door;
+    public GameObject openText;
+
+    public bool inReach;
+
+    void Start()
+    {
+        inReach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (inReach && Input.GetButtonDown("Interact"))
+        {
+            DoorOpens();
+        }
+        else
+        {
+            DoorCloses();
+        }
+    }
+    void DoorOpens ()
+    {
+        door.SetBool("Open", true);
+        door.SetBool("Closed", false);
+
+    }
+
+    void DoorCloses()
+    {
+        door.SetBool("Open", false);
+        door.SetBool("Closed", true);
+    }
+
+}
